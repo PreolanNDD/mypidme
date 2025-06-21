d5ba0cba32d37c361f0c600a761fb9a7a7a8c146
PreolanNDD <147174478+PreolanNDD@users.noreply.github.com>
2025-06-21 12:07:27 +0200

    Updated client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 // lib/supabase.ts
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
 
-export const supabase = createBrowserClient(
+export const createClient = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
