22efdd3c6b13419a52caab4daf24abe25eff5c5b
PreolanNDD <147174478+PreolanNDD@users.noreply.github.com>
2025-06-22 23:06:38 +0200

    Updated Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -50,7 +50,7 @@ export function Sidebar() {
               alt="PIDMe Logo"
               width={320}
               height={120}
-              className="h-24 w-auto rounded-3xl"
+              className="h-24 w-auto rounded-5xl"
             />
           )}
         </div>
